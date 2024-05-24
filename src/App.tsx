6b376e07c854f5fe5df583d638155e806ce8dc5e
Stephan L. Smith <stephan@meenta.io>
2024-05-24 17:01:37 -0400

    Added more goodness

--- src/App.tsx
+++ src/App.tsx
@@ -134,7 +134,8 @@ export function App() {
               <SortBy
                 items={[
                   { value: 'Networking-Book', label: 'Newest to Oldest' },
-                  { value: 'Networking-Book-age-asc', label: 'Oldest to Newest' }
+                  { value: 'Networking-Book-age-asc', label: 'Oldest to Newest' },
+                  { value: 'Networking-Book-calendly-desc', label: 'Last Calendly Mtg' },
                 ]}
               />
               <br />
@@ -247,6 +248,33 @@ const YearsBetween = ({ year }) => {
   return <span>{yearsBetween} years</span>;
 };
 
+function timeAgo(dateString) {
+  const now = new Date();
+  const pastDate = new Date(dateString);
+  const diffInSeconds = Math.floor((now - pastDate) / 1000);
+
+  if (diffInSeconds < 0) return '(Upcoming)'
+
+  const timeIntervals = [
+    { label: 'year', seconds: 31536000 },
+    { label: 'month', seconds: 2592000 },
+    { label: 'week', seconds: 604800 },
+    { label: 'day', seconds: 86400 },
+    { label: 'hour', seconds: 3600 },
+    { label: 'minute', seconds: 60 },
+    { label: 'second', seconds: 1 }
+  ];
+
+  for (const interval of timeIntervals) {
+    const count = Math.floor(diffInSeconds / interval.seconds);
+    if (count > 0) {
+      return `(${count} ${interval.label}${count !== 1 ? 's' : ''} ago)`;
+    }
+  }
+
+  return '';
+}
+
 function Hit({ hit }: HitProps) {
   return (
     <article>
@@ -262,8 +290,7 @@ function Hit({ hit }: HitProps) {
             <Highlight attribute="JobTitle" hit={hit} />
           </p>
         </div>
-        <div className="col-6">
-
+        <div className="col-4">
           <div className="m-2">
             {(hit.Tags || []).map((item, index) => (
               <span key={index} className="badge bg-secondary me-1">
@@ -271,17 +298,35 @@ function Hit({ hit }: HitProps) {
               </span>
             ))}
           </div>
-
           <p>
             <Highlight attribute="Headline" hit={hit} />
           </p>
-
         </div>
-        <div className="col-2">
+        <div className="col-4">
           <p>
-            <Highlight attribute="Company" hit={hit} />
+            <b><Highlight attribute="Company" hit={hit} /></b>
             <br />
             <Highlight attribute="Location" hit={hit} />
+            <br/>
+
+            {hit.DateConnected ? (
+              <>
+              <b>Connected:</b> {hit.DateConnectedStr} {timeAgo(hit.DateConnected)}
+              </>
+            ) : ( 
+              <span>&nbsp;</span>
+            )}
+            
+            <br/>
+
+            {hit.calendly_LastMeetingAt ? (
+              <>
+              <b>Last Mtg:</b> {hit.calendly_LastMeetingAt} {timeAgo(hit.calendly_LastMeetingAt)}
+              </>
+            ) : ( 
+              <span>&nbsp;</span>
+            )}
+          
           </p>
         </div>
       </div>
